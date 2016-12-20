21f35ace48cddcf7cb3b290940e2715c36bd5aab
Chris Chiang <cchiang1992@gmail.com>
2016-12-19 18:04:13 -0800

    Boxes and Box components init

---
+++ src/components/Box.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Box extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className='box'>
+      
+      </div>
+    )
+  }
+}
+
+export default Box;
---
+++ src/components/Boxes.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Boxes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        
+      </div>
+    )
+  }
+}
