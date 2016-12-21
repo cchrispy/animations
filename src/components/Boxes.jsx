b9d2713cb32a55aed367787b6b49d67353abd4cd
Chris Chiang <cchiang1992@gmail.com>
2016-12-20 18:07:53 -0800

    Deleted extra code

--- src/components/Boxes.jsx
+++ src/components/Boxes.jsx
@@ -9,21 +9,6 @@ class Boxes extends Component {
   }
 
   render() {
-    // return (
-    //   <div className='container'>
-    //     <div className='row'>
-    //       <Box color={ 'red' } />
-    //       <Box color={ 'purple' } />
-    //       <Box color={ 'orange' } />
-    //       <Box color={ 'blue' } />
-    //       <Box color={ 'orange' } />
-    //       <Box color={ 'blue' } />
-    //       <Box color={ 'red' } />
-    //       <Box color={ 'purple' } />
-
-    //     </div>
-    //   </div>
-    // )
     return (
       <div className='container'>
         <div className='row'>
